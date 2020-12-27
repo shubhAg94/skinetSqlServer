b0ec8488d7188637a1d2a8818abbb60419703dfa
Shubham Kumar <ska95.dev@gmail.com>
2020-12-27 10:43:36 +0530

    End of sectio 5(Error Handling)

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,18 +1,17 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using API.Dtos;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> _productsRepo;
         private readonly IGenericRepository<ProductBrand> _productBrandRepo;
@@ -20,7 +19,7 @@ namespace API.Controllers
         private readonly IMapper _mapper;
 
         public ProductsController(IGenericRepository<Product> productsRepo,
-            IGenericRepository<ProductBrand> productBrandRepo, 
+            IGenericRepository<ProductBrand> productBrandRepo,
             IGenericRepository<ProductType> productTypeRepo,
             IMapper mapper)
         {
@@ -37,15 +36,19 @@ namespace API.Controllers
 
             var products = await _productsRepo.ListAsync(spec);
 
-            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>> (products));
+            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
             var product = await _productsRepo.GetEntityWithSpec(spec);
+
+            if (product == null) return NotFound(new ApiResponse(404));
 
             return _mapper.Map<Product, ProductToReturnDto>(product);
         }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,13 +1,13 @@
+using API.Extensions;
+using API.Helpers;
+using API.Middleware;
+using AutoMapper;
 using Infrastructure.Data;
-using Core.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using AutoMapper;
-using API.Helpers;
 
 namespace API
 {
@@ -22,7 +22,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //AddTransient(): One of them is called AddTransient() and this one is instantiated for a individual method and not the
+            //AddTransient(): This one is instantiated for a individual method and not the
             //request itself. So it's got a very short lifetime the repository will be created and destroyed just upon using an 
             //individual method.
 
@@ -30,29 +30,33 @@ namespace API
             //application starts and the method goes to the controller and creates a new instance of the repository but 
             //then it would never be destroyed until the application shuts down and that's too long.
 
-            //AddScoped(): So the correct one is the ad scoped option and this is the one that we'll use for almost everything.
+            //AddScoped(): So the correct one is the AddScoped option and this is the one that we'll use for almost everything.
             //So it's gonna be created when the TTP request comes into our API creates a new instance in the controller
             //the controller sees that it needs a repository so it creates the instance of the repository.
             //And when the request is finished then it disposes of both the controller and the repository.
             //And we don't ourselves need to worry about disposing of the resources created when a request comes in.
             //So in order to add our repository we will use AddScoped() and we'll pass in the IProductRepository.
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-
             services.AddAutoMapper(typeof(MappingProfiles));
 
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
             x.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
+
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             //if we call api with http then, firstwe get a 307(temporary redirect) response
             //later we get 200 response from htts assecond response and that is actual response fro api
@@ -63,6 +67,8 @@ namespace API
             app.UseStaticFiles();
 
             app.UseAuthorization();
+
+            app.UseSwaggerDocumentation();
 
             app.UseEndpoints(endpoints =>
             {
