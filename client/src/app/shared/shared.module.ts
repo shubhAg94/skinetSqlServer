c17cadd76b2c8046e2243581864222fcdddb17c2
Shubham Kumar <ska95.dev@gmail.com>
2021-01-02 09:36:30 +0530

    Section 14 Complete - Added Basket Module in the client

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -5,9 +5,10 @@ import {CarouselModule} from 'ngx-bootstrap/carousel';
 import { from } from 'rxjs';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
+import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
 
 @NgModule({
-  declarations: [PagingHeaderComponent, PagerComponent],
+  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
@@ -17,7 +18,8 @@ import { PagerComponent } from './components/pager/pager.component';
     PaginationModule,
     PagingHeaderComponent,
     PagerComponent,
-    CarouselModule
+    CarouselModule,
+    OrderTotalsComponent
   ]
 })
 export class SharedModule { }
